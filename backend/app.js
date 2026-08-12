797bd4a03e01397de63367b53bcc4eae4916c256
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-12 19:19:44 -0400

    setting up the backend

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from 'express'; 
+const app = express();  
+
+app.use(express.json());   
+
+//Routes defined below 
+
+app.get('/overview', (req, res) =>  {
+    res.json({ 
+        Overview: { 
+            ProjectDescription: 
+                `MongoDB + Express.js project focused on understanding and implementing Express.js middleware functions. 
+                Includes seeded data for testing and working with database operations, with Docker integration to reinforce 
+                containerization concepts through hands-on development`
+        }
+    }); 
+}); 
+
+
+// Mount routes below here 
+
+
+export default app; 
