d64d76f37b22b04312d61134b4601f1e8bc800fe
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-15 17:10:49 -0400

    working on the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import app from "./app.js";
-// import database function below here
+// import database function below here 
+import dbConnection from './config/db.connection.js'; 
 // invoke the database function below here
+dbConnection(); 
 
 const port = process.env.PORT;
 
