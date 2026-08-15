0bb7fc060c8bf769e38c30df7ba5ee1b6e82826c
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-15 17:11:22 -0400

    adding a fix here

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 // import database function below here 
-import dbConnection from './config/db.connection.js'; 
+import { dbConnection } from './config/db.connection.js'; 
 // invoke the database function below here
 dbConnection(); 
 
