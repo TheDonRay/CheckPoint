bb059c01f170e9be555df2b200a906f71dfb9bea
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-08-28 11:44:57 -0400

    setting up the user base

---
+++ backend/routes/users.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+// import middleware functions here
+
+// import controllers here below
+
+const userRouter = express.Router();
+
+// route definitions below 
+
+
+export default userRouter;
