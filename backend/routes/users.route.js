c3412ab56cff76f9c3172f0d1619a7b76e2dc43b
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-09-02 17:55:02 -0400

    working on the backend

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,12 +1,15 @@
 import express from 'express';
 
-// import middleware functions here
+// import middleware functions here  
+import {} from '../middleware'; 
 
-// import controllers here below
+// import controllers here below 
+import {userController} from '../controllers/users.controllers'; 
 
 const userRouter = express.Router();
 
-// route definitions below 
+// route definitions below  
+userRouter.get('/all', userController); 
 
 
 export default userRouter;
